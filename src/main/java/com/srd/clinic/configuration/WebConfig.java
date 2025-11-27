0a98f6ba14b7b526bfb0372f8fd0a11cd47200b7
nikho_remulla <n.remulla@ibm.com>
2025-11-27 21:45:36 +0800

    Added CORS config for local testing and prod

---
+++ src/main/java/com/srd/clinic/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.srd.clinic.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "https://srd-clinic-frontend.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+    
+}
